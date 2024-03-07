4f6323423d3c87d37f4f3d4d10bf14dc1bc9f7ef
ChandruMonica2705 <monicamahalingam27@gmail.com>
2024-03-07 19:11:27 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -9,7 +9,7 @@ export default function DashPosts() {
    const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   console.log(userPosts);
-//   const [showMore, setShowMore] = useState(true);
+   const [showMore, setShowMore] = useState(true);
 //   const [showModal, setShowModal] = useState(false);
 //   const [postIdToDelete, setPostIdToDelete] = useState('');
   useEffect(() => {
@@ -19,9 +19,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
-        //   if (data.posts.length < 9) {
-        //     setShowMore(false);
-        //   }
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -32,23 +32,23 @@ export default function DashPosts() {
      }
   }, [currentUser._id]);
 
-//   const handleShowMore = async () => {
-//     const startIndex = userPosts.length;
-//     try {
-//       const res = await fetch(
-//         `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
-//       );
-//       const data = await res.json();
-//       if (res.ok) {
-//         setUserPosts((prev) => [...prev, ...data.posts]);
-//         if (data.posts.length < 9) {
-//           setShowMore(false);
-//         }
-//       }
-//     } catch (error) {
-//       console.log(error.message);
-//     }
-//   };
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
 //   const handleDeletePost = async () => {
 //     setShowModal(false);
@@ -114,6 +114,15 @@ return (
               ))}
             </Table.Body>
           </Table>
+          
+            {showMore && (
+                <button
+                onClick={handleShowMore}
+                  className='w-full text-teal-500 self-center text-sm py-7'
+                >
+                  Show more
+                </button>
+)}
         </>
       ) : (
         <p>You have no posts yet!</p>
